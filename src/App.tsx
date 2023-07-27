9663307695c877a4ff427c70f7c8ceaa891949dd
Ryan DeBerardinis <rdeber@gmail.com>
2023-07-27 16:58:35 -0400

    Update page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState<string>('#bada55')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typography variant="h1" component="div" gutterBottom>
+        Accessible Color Suggestions
+      </Typography>
+      <TextField onChange={handleChange} id="outlined-basic" label="Outlined" variant="outlined" />
+      <Typography variant="h1" component="div" gutterBottom>
+        {color}
+      </Typography>
     </>
   )
 }
